c1852e95a1618457975073e50a308c7ec0ba1782
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-04-16 19:28:27 +0100

    added router.js and router root path for the chatbox app

--- server/index.js
+++ server/index.js
@@ -3,6 +3,14 @@ const socketio = require("socket.io");
 const http = require('http')
 
 
+const Port = process.env.PORT || 5000
+
+const router = require("./router");
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server)
+const io = socketio(server);
+
+app.use(router);
+
+server.listen(Port, () => console.log(`chatBox Server started on port ${Port}`));
+ 
